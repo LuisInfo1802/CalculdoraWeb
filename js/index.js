35fe0d258233a77612bd953f1a44a13d7839b5d4
LuisInfo1802 <luissanchezinfo18@gmail.com>
2023-06-08 18:15:49 -0700

    Validaciones y fin del proyecto

--- js/index.js
+++ js/index.js
@@ -8,6 +8,30 @@ function appendToDisplay(value) {
   
   function calculate() {
     let expression = document.getElementById("display").value;
+    if(!expression)
+    {
+       
+
+  
+    appendAlert('The display is Empy', 'success')
+        return;
+
+    }
     let result = eval(expression);
     document.getElementById("display").value = result;
   }
+
+
+  
+  const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-danger" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
